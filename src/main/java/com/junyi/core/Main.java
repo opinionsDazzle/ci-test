c776a85275ff53b31e5ab7dfd1360d9a266262c5
qkkcoolmax <dingjian.qk@alibaba-inc.com>
2019-05-20 23:10:44 +0800

    init

---
+++ src/main/java/com/junyi/core/Main.java
@@ -0,0 +1,8 @@
+package com.junyi.core;
+
+public class Main {
+    public String main(){
+        System.out.println("hello world");
+        return "hello world return";
+    }
+}
---
+++ src/test/java/com/junyi/core/MainTest.java
@@ -0,0 +1,13 @@
+package com.junyi.core;
+
+public class MainTest {
+
+    private Main main = new Main();
+
+    @org.junit.Test
+    public void main() {
+        System.out.println("test start");
+        main.main();
+        System.out.println("test end");
+    }
+}
